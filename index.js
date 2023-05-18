2c760bd6fd0f5faccfe36d2fd43cbc76cb371e84
SQLAdmin <abuyeahia24@gmail.com>
2023-05-18 14:42:26 +0600

    added categories by mongodb

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const categoryCollection = client.db('toyHunter').collection('categories');
+
+
+        app.get('/categories', async (req, res) => {
+            const cursor = categoryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
