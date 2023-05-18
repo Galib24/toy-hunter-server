bb34d1b95724ef77eb1c95b14612ca9e63105c93
SQLAdmin <abuyeahia24@gmail.com>
2023-05-18 21:58:07 +0600

    category added first and second part

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,11 +34,38 @@ async function run() {
         await client.connect();
 
         const categoryCollection = client.db('toyHunter').collection('categories');
+        const categoryCollection1 = client.db('toyHunter').collection('categories1');
+        const categoryCollection2 = client.db('toyHunter').collection('categories2');
 
 
         app.get('/categories', async (req, res) => {
             const cursor = categoryCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/categories1', async (req, res) => {
+            const cursor = categoryCollection1.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/categories2', async (req, res) => {
+            const cursor = categoryCollection2.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+
+
+            const options = {
+               
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: {  title: 1, price: 1, service_id: 1 },
+            };
+            const result = await categoryCollection.findOne(query,options);
             res.send(result);
         })
 
